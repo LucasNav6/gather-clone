f75a606efcdfc84ba47166b896d63957ed833267
Lucas Navarro <lucas.navarro@lilab.pe>
2025-08-21 19:27:19 -0300

    feat: add title on the list room

--- client/src/rooms/pages/room-list.page.tsx
+++ client/src/rooms/pages/room-list.page.tsx
@@ -1,11 +1,9 @@
-import { Card, CardDescription, CardTitle } from "@/shared/freamwork/shadcn/ui/card"
 import { useGetAssociatedRooms } from "../hooks/useGetAssociatedRooms"
 import { EmptyRoomListPage } from "./empty-room-list.page"
 import { RoomListErrorPage } from "./error-room.page"
 import { LoadingRooms } from "./loading-room.page"
-import { BuildingIcon, Users } from "lucide-react"
+import { BuildingIcon } from "lucide-react"
 import { Button } from "@/shared/freamwork/shadcn/ui/button"
-import { Fragment } from "react"
 
 export const RoomListPage = () => {
     const { isLoading: isRoomsLoading, hasError: hasRoomError, rooms } = useGetAssociatedRooms()
@@ -14,16 +12,19 @@ export const RoomListPage = () => {
     if (isRoomsLoading) return <LoadingRooms />
     if (rooms.length === 0) return <EmptyRoomListPage />
     return (
-        <section className="w-full max-w-3xl mx-auto space-y-4">
+        <section className="w-full max-w-3xl mx-auto space-y-4 p-4">
+            <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight text-balance">
+                Your Rooms
+            </h1>
             {rooms.map((room) => (
-                <article key={room._uuid} className="p-2 border-b last:border-0 flex items-center justify-between">
+                <article key={room._uuid} className="border-b last:border-0 flex items-center justify-between">
                     <div className="flex items-center gap-4">
                         <div className="bg-gray-100 p-2 rounded-md">
                             <BuildingIcon className="w-6 h-6 text-gray-700" />
                         </div>
                         <div className="-space-y-1">
                             <div className="text-lg font-semibold">{room.name}</div>
-                            <p className="text-muted-foreground text-sm">
+                            <p className="text-muted-foreground text-xs">
                                 Members: {room.members.length} | Maps: {room.maps.length}
                             </p>
                         </div>
